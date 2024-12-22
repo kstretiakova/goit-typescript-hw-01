1fc6f8fe469f4dfbec539b8fe84442a1133bd554
Kseniia Tretiakova <ktretyakova1927@gmail.com>
2024-12-22 22:32:55 +0300

    code

--- src/basic/7.ts
+++ src/basic/7.ts
@@ -1,12 +1,28 @@
-interface User {
-    name: string;
-    age: number;
-    email: string;
-    address?: { city: string; country: string };
-}
+type Page = {
+    title: string;
+    likes: number;
+    accounts: string[];
+    status: string;
+    details?: {
+    createAt: Date;
+    updateAt: Date;
+    };
+};
 
-const poly = {
-    name: "Mango",
-    age: 30,
-    email: "john@example.com",
+const page1: Page = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+    },
+};
+
+const page2: Page = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
 };
